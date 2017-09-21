4d51e56e47252bfb122013486e7fcef56c44a9fc
李腾飞 <litengfei@winkind-tech.com>
2017-09-22 01:39:19 +0800

    update

--- src/util/formatter.js
+++ src/util/formatter.js
@@ -1,3 +1,5 @@
+import gps from './gps'
+
 let Formatter = {
     formatter (data, style = "default") {
         var _this = this
@@ -15,32 +17,27 @@ let Formatter = {
     },
     transfer (line, style){
         var _this = this
-        var regexp = /^[1-9]\d*\.?\d*/gi;
+        var regexp = /[1-9]\d*\.?\d*/gi;
         var match;
-        var array = []
-        while(match = regexp.exec(line)){
-            // line.match(regexp)
-            array.push(match[1])
-        }
-         
-        debugger;
+        var array = line.match(regexp)
+
         if (array == null || array.length == 0) {
             return line;
         }
        
         let length = array.length
-        if (length % 2 == 0) {
+        if (length % 2 == 1) {
             return "error:this line data formatter error:" + line;
         }
     
         var results = [];
-        for (i = 0; i < length; i += 2) {
+        for (var i = 0; i < length; i += 2) {
             var item = {
                 lat: Number(array[1]),
                 lon: Number(array[0])
             }
             console.log(item)
-            var p = gps.gcj_decrypt(data.lat, data.lon)
+            var p = gps.gcj_decrypt(item.lat, item.lon)
             results.push(_this.stringify(p, style))
         }
         if (style === "json") {
